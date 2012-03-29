a091bdb6665489d4afd9ec24c0b4273209b92254
unknown <sjohn@sathish.(none)>
2012-03-29 17:33:01 +0400

    Sathish Testing
    Signed-off-by: unknown <sjohn@sathish.(none)>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mokkai::Application.config.secret_token = 'd3cab35b27fdc9871b768612697366ed0e6c091c3fc86c2a8e5482b23b6070475deebea815d33c926cd933d2cb9091ff3f58e5d78cfd326ca394cc94903bfe06'
